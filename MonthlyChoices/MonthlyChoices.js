cdccc4672eaf81d9341cdd1a446cde52f79ba618
Mandy Zou <mz962@mail.missouri.edu>
2017-10-12 16:12:47 -0400

    add animation

--- MonthlyChoices/MonthlyChoices.js
+++ MonthlyChoices/MonthlyChoices.js
@@ -1,19 +1,28 @@
 $( document ).ready(function() {
-    // for toliet people to pop up one by one
-    $(".sf_quote div").each(function(i){
-        var $img = $(this);
+    
+    // For count up animation 01
+    var mcCount01 = new CountUp(document.getElementById("mc-graphics-count1"),0,169);
+    mcCount01.start();
+
+    // For count up animation 02
+    var mcCount02 = new CountUp(document.getElementById("mc-graphics-count2"),0,171);
+    mcCount02.start();
+
+    // For count up animation 03
+    var mcCount03 = new CountUp(document.getElementById("mc-graphics-count3"),0,183);
+    mcCount03.start();
+
+    // For count up animation 04
+    var mcCount04 = new CountUp(document.getElementById("mc-graphics-count4"),0,200);
+    mcCount04.start();
+    
+
+    $(".mc-graphics").each(function(i){
+        var $mcImg = $(this);
         setTimeout(function(){
-            $img.addClass("slideUp");
+            $mcImg.addClass("bigEntrance");
         }, i*500);
     });
-
-    // function addPop(){
-    //     $(".numbers-food-insecurity").addClass("slideUp");
-    // };
-
-    // $(".numbers-food-insecurity").addClass(function(){
-    //     setTimeout(addPop, 5000);
-    // });
 
 });
 
