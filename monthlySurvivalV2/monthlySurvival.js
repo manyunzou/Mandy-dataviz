6b230a4db706607487bfbd7a28e298f72bd66263
Mandy Zou <mz962@mail.missouri.edu>
2017-10-19 15:41:14 -0400

    change hover over

--- monthlySurvivalV2/monthlySurvival.js
+++ monthlySurvivalV2/monthlySurvival.js
@@ -1,4 +1,19 @@
 // Resources: https://stackoverflow.com/questions/26428341/how-can-i-add-different-images-instead-of-text-inside-d3-js-piechart-slices
 
+$(document).ready(function(){
 
+    $(".pHousing").mouseover(function(){
+        $(".pHousing-text").css({
+            "color":"#fff",
+            "background-color":"#762056"
+        });
+    });
 
+    $(".pHousing").mouseout(function(){
+        $(".pHousing-text").css({
+            "color":"#f2672a",
+            "background-color":"none"
+        });
+    });
+
+});
