2d6afa209a18ac3ffd7fa22ea4b737e158e2b89e
Mandy Zou <mz962@mail.missouri.edu>
2017-10-19 18:57:06 -0400

    change css effect

--- monthlySurvivalV2/monthlySurvival.js
+++ monthlySurvivalV2/monthlySurvival.js
@@ -6,13 +6,6 @@ $("#budget-person-numbers").addClass("animated fadeInLeft");
 
 $("#budget-family-numbers").addClass("animated fadeInLeft");
 
-var waypoint = new Waypoint({
-    element: document.getElementById("chart_family"),
-    handler: function(){
-        $("#chart_family").addClass("animated rotateIn")
-    }
-});
-
 //p-Housing hover over
     $(".pHousing").mouseover(function(){
         $(".pHousing-text").css({
