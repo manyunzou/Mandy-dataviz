2eced336e080136b979a2b37b00064a27ed92315
Mandy Zou <mz962@mail.missouri.edu>
2017-10-23 11:46:43 -0400

    Add all overview section

---
+++ issue01/js/Hardships.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    
+    // For count up animation 01
+    var coinCount01 = new CountUp(document.getElementById("hs_numberCount01"),0,31);
+    coinCount01.start();
+
+    // For count up animation 02
+    var coinCount02 = new CountUp(document.getElementById("hs_numberCount02"),0,49);
+    coinCount02.start();
+
+    // For count up animation 03
+    var coinCount03 = new CountUp(document.getElementById("hs_numberCount03"),0,14);
+    coinCount03.start();
+    
+
+});
+
