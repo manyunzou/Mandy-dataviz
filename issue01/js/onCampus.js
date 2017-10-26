2dca423baed123bae9c327e9936727ddbfdf1fa5
Mandy Zou <mz962@mail.missouri.edu>
2017-10-26 09:53:08 -0500

    add map interaction

---
+++ issue01/js/onCampus.js
@@ -0,0 +1,90 @@
+$( document ).ready(function() {
+    
+    $(".virginia-map-p1").mouseover(function(){
+        $("#virginia-text-p1").css({
+            "background-color":"#762056",
+            "color":"#fff",
+        })
+    });
+
+    $(".virginia-map-p1").mouseout(function(){
+        $("#virginia-text-p1").css({
+            "background-color":"#fff",
+            "color":"#000",
+        })
+    });
+
+    $(".virginia-map-p2").mouseover(function(){
+        $("#virginia-text-p2").css({
+            "background-color":"#762056",
+            "color":"#fff",
+        })
+    });
+
+    $(".virginia-map-p2").mouseout(function(){
+        $("#virginia-text-p2").css({
+            "background-color":"#fff",
+            "color":"#000",
+        })
+    });
+
+    $(".virginia-map-p3").mouseover(function(){
+        $("#virginia-text-p3").css({
+            "background-color":"#762056",
+            "color":"#fff",
+        })
+    });
+
+    $(".virginia-map-p3").mouseout(function(){
+        $("#virginia-text-p3").css({
+            "background-color":"#fff",
+            "color":"#000",
+        })
+    });
+
+    $(".virginia-map-p4").mouseover(function(){
+        $("#virginia-text-p4").css({
+            "background-color":"#762056",
+            "color":"#fff",
+        })
+    });
+
+    $(".virginia-map-p4").mouseout(function(){
+        $("#virginia-text-p4").css({
+            "background-color":"#fff",
+            "color":"#000",
+        })
+    });
+
+    $(".virginia-map-p5").mouseover(function(){
+        $("#virginia-text-p5").css({
+            "background-color":"#762056",
+            "color":"#fff",
+        })
+    });
+
+    $(".virginia-map-p5").mouseout(function(){
+        $("#virginia-text-p5").css({
+            "background-color":"#fff",
+            "color":"#000",
+        })
+    });
+
+    $(".virginia-map-p6").mouseover(function(){
+        $("#virginia-text-p6").css({
+            "background-color":"#762056",
+            "color":"#fff",
+        })
+    });
+
+    $(".virginia-map-p6").mouseout(function(){
+        $("#virginia-text-p6").css({
+            "background-color":"#fff",
+            "color":"#000",
+        })
+    });
+    
+    
+
+});
+
