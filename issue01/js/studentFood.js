c1d6ba6b303321ca1710a40fb2b68f4325576920
Mandy Zou <mz962@mail.missouri.edu>
2017-10-26 09:12:27 -0500

    add animation to student & food

--- issue01/js/studentFood.js
+++ issue01/js/studentFood.js
@@ -1,6 +1,11 @@
 $( document ).ready(function() {
     
-    
+    $(".sf_quote div").each(function(i){
+        var $img = $(this);
+        setTimeout(function(){
+            $img.addClass("slideUp");
+        }, i*500);
+    });
     
 
 });
