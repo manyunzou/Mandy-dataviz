769dfe4be1afa567d4d1e780c1942f6fc6ff2fd9
Mandy Zou <mz962@mail.missouri.edu>
2017-11-03 12:21:52 -0400

    add white background

--- issue01V2/js/Hardships.js
+++ issue01V2/js/Hardships.js
@@ -1,11 +1,5 @@
-$(window).scroll(function() {
+$( document ).ready(function() {
     
-    var     hT = $("#Issue01-hardships").offset().top,
-            hH = $("#Issue01-studentFood").outerHeight(),
-            wH = $(window).height(),
-            wS = $(this).scrollTop();
-
-        if (hT > wS){
             // For count up animation 01
             var coinCount01 = new CountUp(document.getElementById("hs_numberCount01"),0,31);
             coinCount01.start();
@@ -17,7 +11,6 @@ $(window).scroll(function() {
             // For count up animation 03
             var coinCount03 = new CountUp(document.getElementById("hs_numberCount03"),0,14);
             coinCount03.start();
-        };
 
 });
 
