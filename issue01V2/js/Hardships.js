796b1169bd93f690a293d601025d49ac66ecfee2
Mandy Zou <mz962@mail.missouri.edu>
2017-11-06 10:40:38 -0500

    update hunger & monthly budget

--- issue01V2/js/Hardships.js
+++ issue01V2/js/Hardships.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     
             // For count up animation 01
-            var coinCount01 = new CountUp(document.getElementById("hs_numberCount01"),0,31);
+            var coinCount01 = new CountUp(document.getElementById("hs_numberCount01"),0,38);
             coinCount01.start();
 
             // For count up animation 02
@@ -9,7 +9,7 @@ $( document ).ready(function() {
             coinCount02.start();
 
             // For count up animation 03
-            var coinCount03 = new CountUp(document.getElementById("hs_numberCount03"),0,14);
+            var coinCount03 = new CountUp(document.getElementById("hs_numberCount03"),0,15);
             coinCount03.start();
 
 });
