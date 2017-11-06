2682f48751e650b851e64968a1e5d1071fb96fdd
Mandy Zou <mz962@mail.missouri.edu>
2017-11-06 14:23:29 -0500

    add hardworking students

---
+++ issue01V2/js/hwStudents.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    // For count up animation 01
+    var coinCount01 = new CountUp(document.getElementById("coins-count1"),0,56);
+    coinCount01.start();
+
+    // For count up animation 02
+    var coinCount02 = new CountUp(document.getElementById("coins-count2"),0, 38);
+    coinCount02.start();
+
+    // For count up animation 03
+    var coinCount03 = new CountUp(document.getElementById("coins-count3"), 0, 75);
+    coinCount03.start();
+
+    // For count up animation 04
+    var coinCount04 = new CountUp(document.getElementById("coins-count4"), 0, 52);
+    coinCount04.start();
+});
