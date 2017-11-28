af4d006582a2df4c2ccef6cc3ec8385db389f304
Mandy Zou <mz962@mail.missouri.edu>
2017-11-27 23:15:01 -0500

    Edit by the number

--- issue02/js/bytheNumbers.js
+++ issue02/js/bytheNumbers.js
@@ -1,28 +1,28 @@
 $( document ).ready(function() {
 
 
-    // for toliet people to pop up one by one
-    $(".numbers-people-chart img").each(function(i){
-        var $img = $(this);
-        setTimeout(function(){
-            $img.addClass("bigEntrance");
-        }, i*200);
-    });
+    // // for toliet people to pop up one by one
+    // $(".numbers-people-chart img").each(function(i){
+    //     var $img = $(this);
+    //     setTimeout(function(){
+    //         $img.addClass("bigEntrance");
+    //     }, i*200);
+    // });
 
-    // For count up animation 01
-    var coinCount01 = new CountUp(document.getElementById("numbers-count1"),0,20, 0, 9);
-    coinCount01.start();
+    // // For count up animation 01
+    // var coinCount01 = new CountUp(document.getElementById("numbers-count1"),0,20, 0, 9);
+    // coinCount01.start();
 
-    // For count up animation 02
-    var coinCount02 = new CountUp(document.getElementById("numbers-count2"),0, 35, 0, 9);
-    coinCount02.start();
+    // // For count up animation 02
+    // var coinCount02 = new CountUp(document.getElementById("numbers-count2"),0, 35, 0, 9);
+    // coinCount02.start();
 
-    function addPop(){
-        $(".numbers-food-insecurity").addClass("bigEntrance");
-    };
+    // function addPop(){
+    //     $(".numbers-food-insecurity").addClass("bigEntrance");
+    // };
 
-    $(".numbers-food-insecurity").addClass(function(){
-        setTimeout(addPop, 3000);
-    });
+    // $(".numbers-food-insecurity").addClass(function(){
+    //     setTimeout(addPop, 3000);
+    // });
 
 });
